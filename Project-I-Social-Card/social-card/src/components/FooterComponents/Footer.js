e11d8a8ad0ed672377e043caa1f1e4ab603893c1
Greg Johnston <johng3269@gmail.com>
2019-07-09 19:04:46 -0400

    Working on header

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,11 +3,13 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <p><i class="far fa-comment"></i></p>
-      <p><i class="fas fa-retweet"></i></p>
-      <p><i class="far fa-heart"></i></p>
-      <p><i class="far fa-envelope"></i></p>
+    <div className="footer-div">
+      <div className = "footer">
+        <p className = 'comment'><i class="far fa-comment"></i></p>
+        <p className = 'retweet'><i class="fas fa-retweet"></i></p>
+        <p className = 'heart'><i class="far fa-heart"></i></p>
+        <p className = 'envelope'><i class="far fa-envelope"></i></p>
+      </div>
     </div>
   );
 };
